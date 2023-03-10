64425b9df70062e56320d86fef9580c5b148f68c
nicole <nicolenamayan133@gmail.com>
2023-03-10 21:45:20 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const spy='hello'
+console.log('hello')
+{
+    function sayHiToHeadphonedRoomate (string){
+     if(string.toUpperCase){return "YES INDEED!"}
+
+     else if (string.toLowerCase) {return "Ican't hear you"}
+
+     else if (string("let's have dinner together!")){return "I would love to!"}
+
+
+    }
+}
